1c445d4bd8350fe8a4503f95bfc6d5cc16ae5c3d
Bubbles <bubbles@pop-os.localdomain>
2023-12-10 21:41:27 -0500

    works

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 
@@ -49,9 +50,9 @@ func ExportJSON(rec Recipe) (string, error) {
 func InsertDb(db *sql.DB, data Recipe) { //accept JSON struct instead
 
 	_, err := db.Exec(`
-	INSERT INTO recipes (name, steps, ingredients)
-	VALUES (
-		$1, $2, $3 
+		INSERT INTO recipes (name, steps, ingredients)
+		VALUES (
+			$1, $2, $3 
 	);`, data.Name, data.Steps, pq.Array(data.Ingredients))
 
 	if err != nil {
@@ -75,8 +76,47 @@ func CreateDb(db *sql.DB) {
 	}
 }
 
+func QueryDb(db *sql.DB, name string) (Recipe, error) {
+	rec := Recipe{}
+
+	rows, err := db.Query(`
+		SELECT name, ingredients, steps FROM recipes WHERE name = $1 
+	`, name)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() { //Boolean returning function that checks if there is another row and points rows.Scan() to it
+		err := rows.Scan(&rec.Name, pq.Array(&rec.Ingredients), &rec.Steps)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return rec, nil
+}
+
 func main() {
 
+	var help_flag = flag.Bool("help", false, "Show Help")
+	var import_flag string = "None"
+	var export_flag string = "None"
+	var create_flag string = "None"
+	var recipe_name_flag string = "None"
+
+	flag.StringVar(&import_flag, "import", "None", "Imports data from JSON file to PostgreSQL. Arguement value is the name of the file")
+	flag.StringVar(&export_flag, "export", "None", "Exports data from PostgreSQL to JSON File. Arguement value is the name of the file. recipe name needs to be provided.")
+	flag.StringVar(&create_flag, "create", "None", "Creates the database on the PostgreSQL Server.")
+	flag.StringVar(&recipe_name_flag, "recipe", "None", "Gets recipe name as input for export.")
+
+	flag.Parse()
+
+	if *help_flag {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	data := Recipe{}
 	var psqlData string = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlData)
@@ -84,15 +124,30 @@ func main() {
 		panic(err)
 	}
 
-	CreateDb(db)
+	if create_flag != "None" {
+		CreateDb(db)
+	}
+	if import_flag != "None" {
+		var err = ImportJSON(import_flag, &data)
+		if err != nil {
+			panic(err)
+		}
+		InsertDb(db, data)
+	}
+	if export_flag != "None" || recipe_name_flag != "None" {
+		var recData Recipe
+		recData, err = QueryDb(db, recipe_name_flag)
+		if err != nil {
+			panic(err)
+		}
+		var json, err = ExportJSON(recData)
+		if err != nil {
+			panic(err)
+		}
 
-	data := Recipe{}
+		os.WriteFile(export_flag, []byte(json), 0644)
 
-	ImportJSON("test.json", &data)
-
-	fmt.Println(data)
-
-	InsertDb(db, data)
+	}
 
 	defer db.Close()
 }
